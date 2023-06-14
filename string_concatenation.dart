7eeb409e53bf35e1c5e421ae10c3d14a847ecdaa
Ei Shwe Sin Win <shwesin111info@gmail.com>
2023-06-14 19:52:24 +0630

    string

---
+++ string_concatenation.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Ei Shwe Sin Win';
+  int age = 23, height = 23;
+
+  print("My nmae is $name.I'm $age years old.I'm $height feet tall");
+  print("Next year I will be ${age + 1} years old");
+}
